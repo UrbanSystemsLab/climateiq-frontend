bfb0dbaddad76a52e8b11b6235c15331c0fb23ae
Sunny Mak <smak@google.com>
2024-06-12 21:24:52 +0000

    Update to address comments.

--- cloud_functions/climateiq_merge_output_cf/main.py
+++ cloud_functions/climateiq_merge_output_cf/main.py
@@ -3,5 +3,5 @@ import functions_framework
 
 
 @functions_framework.cloud_event
-def merge_predictions(cloud_event: http.CloudEvent) -> None:
+def merge_scenario_predictions(cloud_event: http.CloudEvent) -> None:
     return
