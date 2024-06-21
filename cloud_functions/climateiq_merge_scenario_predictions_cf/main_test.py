357e398b7ffe172dd6e320398e018dd82539978d
Sunny Mak <smak@google.com>
2024-06-21 21:55:21 +0000

    Update to address comments.

--- cloud_functions/climateiq_merge_scenario_predictions_cf/main_test.py
+++ cloud_functions/climateiq_merge_scenario_predictions_cf/main_test.py
@@ -62,10 +62,6 @@ def test_merge_scenario_predictions(mock_storage_client, tmpdir, app) -> None:
         "run/flood/model/nyc/scenario0/chunk1.csv": _create_chunk_file(
             {"h310": 0.10, "h311": 0.11}, tmpdir
         ),
-        # Chunk only exists in one scenario
-        "run/flood/model/nyc/scenario0/chunk2.csv": _create_chunk_file(
-            {"h320": 0.20, "h321": 0.21}, tmpdir
-        ),
         # Chunk doesn't match file name pattern
         "run/flood/model/nyc/scenario0/ignore/chunk0.csv": _create_chunk_file(
             {"h300": 0.99, "h301": 9.99}, tmpdir
@@ -77,9 +73,66 @@ def test_merge_scenario_predictions(mock_storage_client, tmpdir, app) -> None:
         "run/flood/model/nyc/scenario1/chunk1.csv": _create_chunk_file(
             {"h310": 1.10, "h311": 1.11, "h312": 1.12}, tmpdir
         ),
-        # Scenario only exists for one chunk
-        "run/flood/model/nyc/scenario2/chunk0.csv": _create_chunk_file(
-            {"h300": 2.00, "h301": 2.01}, tmpdir
+        "merged/run/nyc/flood/chunk0.csv": tempfile.NamedTemporaryFile(
+            "w+", dir=tmpdir.name, delete=False
+        ),
+        "merged/run/nyc/flood/chunk1.csv": tempfile.NamedTemporaryFile(
+            "w+", dir=tmpdir.name, delete=False
+        ),
+    }
+    blobs = {name: _create_mock_blob(name, file) for name, file in files.items()}
+    mock_storage_client().bucket.return_value = _create_mock_bucket(blobs)
+    mock_storage_client().list_blobs.side_effect = lambda _, prefix: [
+        blob for name, blob in blobs.items() if name.startswith(prefix)
+    ]
+    with app.test_request_context(
+        query_string={
+            "run_id": "run",
+            "prediction_type": "flood",
+            "model_id": "model",
+            "study_area_name": "nyc",
+        }
+    ):
+        result = main.merge_scenario_predictions(flask.request)
+        assert result == ("Success", 200)
+
+        expected_chunk0_contents = [
+            "h3_index,scenario0,scenario1\n",
+            "h300,0.0,1.0\n",
+            "h301,0.01,1.01\n",
+        ]
+        expected_chunk1_contents = [
+            "h3_index,scenario0,scenario1\n",
+            "h310,0.1,1.1\n",
+            "h311,0.11,1.11\n",
+            "h312,,1.12\n",
+        ]
+        with open(files["merged/run/nyc/flood/chunk0.csv"].name) as fd:
+            assert fd.readlines() == expected_chunk0_contents
+        with open(files["merged/run/nyc/flood/chunk1.csv"].name) as fd:
+            assert fd.readlines() == expected_chunk1_contents
+
+
+@mock.patch.object(storage, "Client", autospec=True)
+def test_merge_scenario_predictions_missing_chunk_raises_error(
+    mock_storage_client, tmpdir, app
+) -> None:
+    files = {
+        "run/flood/model/nyc/scenario0/chunk0.csv": _create_chunk_file(
+            {"h300": 0.00, "h301": 0.01}, tmpdir
+        ),
+        "run/flood/model/nyc/scenario0/chunk1.csv": _create_chunk_file(
+            {"h310": 0.10, "h311": 0.11}, tmpdir
+        ),
+        # Chunk only exists in one scenario
+        "run/flood/model/nyc/scenario0/chunk2.csv": _create_chunk_file(
+            {"h320": 0.20, "h321": 0.21}, tmpdir
+        ),
+        "run/flood/model/nyc/scenario1/chunk0.csv": _create_chunk_file(
+            {"h300": 1.00, "h301": 1.01}, tmpdir
+        ),
+        "run/flood/model/nyc/scenario1/chunk1.csv": _create_chunk_file(
+            {"h310": 1.10, "h311": 1.11}, tmpdir
         ),
         "merged/run/nyc/flood/chunk0.csv": tempfile.NamedTemporaryFile(
             "w+", dir=tmpdir.name, delete=False
@@ -105,30 +158,58 @@ def test_merge_scenario_predictions(mock_storage_client, tmpdir, app) -> None:
         }
     ):
         result = main.merge_scenario_predictions(flask.request)
-        assert result == ("Success", 200)
+        assert result == (
+            "Not found: Missing predictions for run/flood/model/nyc/scenario1/chunk2.csv",
+            404,
+        )
 
-        expected_chunk0_contents = [
-            "h3_index,scenario0,scenario1,scenario2\n",
-            "h300,0.0,1.0,2.0\n",
-            "h301,0.01,1.01,2.01\n",
-        ]
-        expected_chunk1_contents = [
-            "h3_index,scenario0,scenario1,scenario2\n",
-            "h310,0.1,1.1,\n",
-            "h311,0.11,1.11,\n",
-            "h312,,1.12,\n",
-        ]
-        expected_chunk2_contents = [
-            "h3_index,scenario0,scenario1,scenario2\n",
-            "h320,0.2,,\n",
-            "h321,0.21,,\n",
-        ]
-        with open(files["merged/run/nyc/flood/chunk0.csv"].name) as fd:
-            assert fd.readlines() == expected_chunk0_contents
-        with open(files["merged/run/nyc/flood/chunk1.csv"].name) as fd:
-            assert fd.readlines() == expected_chunk1_contents
-        with open(files["merged/run/nyc/flood/chunk2.csv"].name) as fd:
-            assert fd.readlines() == expected_chunk2_contents
+
+@mock.patch.object(storage, "Client", autospec=True)
+def test_merge_scenario_predictions_missing_scenario_raises_error(
+    mock_storage_client, tmpdir, app
+) -> None:
+    files = {
+        "run/flood/model/nyc/scenario0/chunk0.csv": _create_chunk_file(
+            {"h300": 0.00, "h301": 0.01}, tmpdir
+        ),
+        "run/flood/model/nyc/scenario0/chunk1.csv": _create_chunk_file(
+            {"h310": 0.10, "h311": 0.11}, tmpdir
+        ),
+        "run/flood/model/nyc/scenario1/chunk0.csv": _create_chunk_file(
+            {"h300": 1.00, "h301": 1.01}, tmpdir
+        ),
+        "run/flood/model/nyc/scenario1/chunk1.csv": _create_chunk_file(
+            {"h310": 1.10, "h311": 1.11}, tmpdir
+        ),
+        # Scenario only exists for one chunk
+        "run/flood/model/nyc/scenario2/chunk0.csv": _create_chunk_file(
+            {"h300": 2.00, "h301": 2.01}, tmpdir
+        ),
+        "merged/run/nyc/flood/chunk0.csv": tempfile.NamedTemporaryFile(
+            "w+", dir=tmpdir.name, delete=False
+        ),
+        "merged/run/nyc/flood/chunk1.csv": tempfile.NamedTemporaryFile(
+            "w+", dir=tmpdir.name, delete=False
+        ),
+    }
+    blobs = {name: _create_mock_blob(name, file) for name, file in files.items()}
+    mock_storage_client().bucket.return_value = _create_mock_bucket(blobs)
+    mock_storage_client().list_blobs.side_effect = lambda _, prefix: [
+        blob for name, blob in blobs.items() if name.startswith(prefix)
+    ]
+    with app.test_request_context(
+        query_string={
+            "run_id": "run",
+            "prediction_type": "flood",
+            "model_id": "model",
+            "study_area_name": "nyc",
+        }
+    ):
+        result = main.merge_scenario_predictions(flask.request)
+        assert result == (
+            "Not found: Missing predictions for run/flood/model/nyc/scenario2/chunk1.csv",
+            404,
+        )
 
 
 @pytest.mark.parametrize(
