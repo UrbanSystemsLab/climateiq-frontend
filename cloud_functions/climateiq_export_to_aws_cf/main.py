2701119ac20daa03d5cd8f57ebb6f72a54a821ed
Sunny Mak <smak@google.com>
2024-07-19 18:07:10 +0000

    Read AWS secrets from env instead.

--- cloud_functions/climateiq_export_to_aws_cf/main.py
+++ cloud_functions/climateiq_export_to_aws_cf/main.py
@@ -4,8 +4,7 @@ import os
 import boto3
 import flask
 import functions_framework
-from google.cloud import secretmanager
-from google.cloud.storage import client as gcs_client
+from google.cloud import storage
 
 # GCS bucket name, where merged prediction outputs are stored.
 GCS_BUCKET_NAME = (
@@ -15,7 +14,6 @@ GCS_BUCKET_NAME = (
 S3_BUCKET_NAME = "climateiq-data-delivery"
 
 # IDs for retrieving secrets to authenticate to AWS
-PROJECT_ID = "climateiq"
 AWS_ACCESS_KEY_ID = "climasens-aws-access-key-id"
 AWS_SECRET_ACCESS_KEY = "climasens-aws-secret-access-key"
 
@@ -27,20 +25,16 @@ def export_to_aws(request: flask.Request) -> tuple[str, int]:
     except ValueError as e:
         return (str(e), 400)
 
-    storage_client = gcs_client.Client()
-    blobs_to_export = storage_client.list_blobs(GCS_BUCKET_NAME, prefix=prefix)
+    storage_client = storage.Client()
+    blobs_to_export = list(storage_client.list_blobs(GCS_BUCKET_NAME, prefix=prefix))
 
     if not len(blobs_to_export):
-        return (f"No blobs found with prefix {prefix}", 200)
+        return (f"No blobs found with prefix {prefix}", 400)
 
     output_file_dir = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 
-    secrets_client = secretmanager.SecretManagerServiceClient()
-    try:
-        aws_access_key_id = _get_secret_data(secrets_client, AWS_ACCESS_KEY_ID)
-        aws_secret_access_key = _get_secret_data(secrets_client, AWS_SECRET_ACCESS_KEY)
-    except ValueError as e:
-        return (str(e), 500)
+    aws_access_key_id = os.environ.get(AWS_ACCESS_KEY_ID)
+    aws_secret_access_key = os.environ.get(AWS_SECRET_ACCESS_KEY)
 
     s3_client = boto3.client(
         "s3",
@@ -60,19 +54,3 @@ def _get_prefix_id(request: flask.Request) -> str:
     if not prefix:
         raise ValueError("No prefix provided in request parameters.")
     return prefix
-
-
-def _get_secret_data(
-    client: secretmanager.SecretManagerServiceClient, secret_id: str
-) -> str:
-    versions = client.list_secret_versions(
-        parent=client.secret_path(PROJECT_ID, secret_id)
-    )
-    try:
-        latest_enabled_version = next(
-            version.name for version in versions if version.state == 1
-        )
-    except StopIteration:
-        raise ValueError(f"No enabled versions for found secret {secret_id}.")
-    response = client.access_secret_version(name=latest_enabled_version)
-    return response.payload.data.decode("UTF-8")
